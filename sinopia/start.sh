4d8885995265d9779fb4e201dda064a63e318b87
Christopher Dowell <cbdowell@gmail.com>
2017-04-10 17:12:03 -0700

    / ‘sinopia/start.sh’

--- sinopia/start.sh
+++ sinopia/start.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env bash
 
-docker run --name sinopia -d -p 4873:4873 keyvanfatehi/sinopia:latest
-
-# Mount the config file to the exposed data volume
 docker run \
-    -v /path/to/config.yaml:/opt/sinopia/config.yaml \
+    -v ./config.yaml:/opt/sinopia/config.yaml \
     --name sinopia \
     -d \
     -p 4873:4873 \
