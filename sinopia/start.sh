1fc2a0f0ed02ce3ba66bc3533fcdbb5964dac6f6
Christopher Dowell <cbdowell@gmail.com>
2017-04-10 17:36:04 -0700

    / ‘sinopia/start.sh’

--- sinopia/start.sh
+++ sinopia/start.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-docker run \
-    -v /home/chris/SparkleShare/microbe-docker/sinopia/config.yaml:/opt/sinopia/config.yaml \
-    --name sinopia \
-    -d \
-    -p 4873:4873 \
-    keyvanfatehi/sinopia:latest
+# docker run \
+#     -v /home/chris/SparkleShare/microbe-docker/sinopia/config.yaml:/opt/sinopia/config.yaml \
+#     --name sinopia \
+#     -d \
+#     -p 4873:4873 \
+#     keyvanfatehi/sinopia:latest
